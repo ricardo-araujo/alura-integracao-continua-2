afaef680aded0ceeeb95a278f19066289df481f6
Ricardo Araujo <ricardo.araujo@forseti.com.br>
2026-06-22 21:43:47 -0300

    Removendo versões antigas de ubuntu

--- database/db.go
+++ database/db.go
@@ -2,7 +2,7 @@ package database
 
 import (
 	"log"
-
+	"os"
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,7 +14,14 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+
+	host := os.Getenv("HOST")
+	user := os.Getenv("USER")
+	pass := os.Getenv("PASSWORD")
+	db   := os.Getenv("DATABASE")
+	port := os.Getenv("PORT")
+
+	stringDeConexao := "host=" + host + "user=" + user + "password=" + pass + "dbname=" + db + "port=" + port + "sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
